a98d7ce06cc4b1ba3e1169c6ed1e57401e23807a
LorenzoBertarelli <lorenzobertarelli59@gmail.com>
2023-03-27 16:26:59 +0200

    fine bundle 1

---
+++ bundle-1/main.js
@@ -0,0 +1,76 @@
+/*******************************************************************************
+
+    Per ognuno dei seguenti esercizi, rispondi a queste domande:
+    1- Che cosa fa questo codice?
+    2- Sono presenti errori di sintassi?
+    3- Sono presenti errori logici?
+
+    Inserisci i tuoi commenti con le risposte, sotto ciascun esercizio
+
+*******************************************************************************/
+
+
+// // ESERCIZIO 1
+// function checkAge() {
+//     const myAge = 32;
+//     let message = '';
+
+//     if (myAge < 18) {
+//         message = `Sei troppo giovane! Hai ${myAge} anni!`
+//     } else {
+//         message = 'Hai più di 18 anni!'
+//     }
+//     return message;
+// };
+// console.log(checkAge());
+
+// // Punti e virgola di troppo nella funzione e mancanza di return e aggiunta console.log()
+
+
+// // ESERCIZIO 2
+// function printColorsNumber() {
+//     const colors = ['blue', 'red', 'yellow', 'green', 'black'];
+//     console.log(`Nella mia palette ci sono ${colors} colori!`);
+// }
+// printColorsNumber();
+
+// // nel console.log() non era necessario inserire .length per prendere tutti i colori
+
+
+// // ESERCIZIO 3
+// function addNumbers() {
+//     const userNumber = parseInt(prompt('Inserisci un numero'));
+//     const total = userNumber + 12;
+
+//     console.log(`Il risultato finale è ${total}`);
+// }
+// addNumbers();
+
+// // mancava parseInt per trasformare la stringa in numero
+
+
+// // ESERCIZIO 4
+// function checkAccess() {
+//     const addresses = [
+//         'mymail@mail.com',
+//         'yourmail@mail.com',
+//         'hermail@mail.com',
+//         'hismail@mail.com',
+//     ];
+//     const userEmail = prompt('Inserisci il tuo indirizzo email');
+
+//     let grantAccess = false;
+
+//     if (addresses.includes(userEmail)) {
+//         grantAccess = true;
+//     }
+
+//     if (grantAccess === true) {
+//         console.log('Accesso consentito!');
+//     } else {
+//         console.log('Accesso negato!');
+//     }
+// }
+// checkAccess();
+
+// // I TERMINI BOOLEANI NON VANNO INSERITI ALL'INTERNO DEGLI APICI ``
